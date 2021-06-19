388071f71758f5b8c330e5b5bf8e61b5e36a3bfd
woodbully <60384319+woodbully@users.noreply.github.com>
2021-06-19 21:12:52 +0200

    puha

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
 "use strict";
 
 // Get the modal
-var modal = document.getElementById("myModal");
+let modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+let btn = document.querySelectorAll(".myBtn").forEach(item);
+for (item of items) {
+    btn.onclick = function () {
+        modal.style.display = "block";
+    };
+}
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+let span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
